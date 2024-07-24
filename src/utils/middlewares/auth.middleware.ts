aaa883e103422e7675cc31ca241010347a4d922b
Tanishq <tanishq.jaiswal97@gmail.com>
2024-07-24 12:49:08 +0530

    Updated docs

--- src/utils/middlewares/auth.middleware.ts
+++ src/utils/middlewares/auth.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(' ')[1];
-  if (token === 'mGvwy6P7pE2SNN6iaExpKjIGBGUfZpLW') {
+  if (token === process.env.BEARER_TOKEN) {
     next();
   } else {
     res.status(401).json({ message: 'Unauthorized' });
